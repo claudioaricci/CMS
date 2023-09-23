604e42be83b8bb92b2b2c6e0ec25d32ad1b3245d
claudioaricci <claudioaricci@gmail.com>
2023-09-23 15:50:23 -0300

    feat: Consumindo dados do CMS Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,30 @@ import Head from "next/head";
 import styles from "../styles/home.module.scss";
 import Image from "next/image";
 import techsImage from "../../public/images/techs.svg";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+import { getPrismicClient } from "../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+type Content = {
+  title: string;
+  titleContent: string;
+  linkAction: string;
+  mobileTitle: string;
+  mobileContent: string;
+  mobileBanner: string;
+  webTitle: string;
+  webContent: string;
+  webBanner: string;
+};
+
+interface ContentProps {
+  content: Content;
+}
+
+export default function Home({ content }: ContentProps) {
+  console.log("content", content);
   return (
     <>
       <Head>
@@ -12,7 +34,7 @@ export default function Home() {
       <main className={styles.container}>
         <div className={styles.containerHeader}>
           <section className={styles.ctaText}>
-            <h1>Levando você ao próximo nível!</h1>
+            <h1>{content?.title}</h1>
             <span>
               Uma plataforma com cursos que vão do zero até o profissional na
               pratica, direto ao ponto aplicando o que usamos no mercado de
@@ -68,3 +90,44 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at("document.type", "home"),
+  ]);
+
+  //console.log(response.results[0].data)
+
+  const {
+    title,
+    sub_title,
+    link_action,
+    mobile,
+    mobile_content,
+    mobile_banner,
+    title_web,
+    web_content,
+    web_banner,
+  } = response.results[0].data;
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url,
+  };
+
+  return {
+    props: {
+      content,
+    },
+    revalidate: 60 * 2, // A cada 2 minutos
+  };
+};
